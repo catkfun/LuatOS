9b5a36ff6623de2b3594648d6b961473a553993d
CodeRipper <kai19961201@gmail.com>
2022-04-04 11:12:58 +0800

    Update lv_conf.h

--- components/lvgl/lv_conf.h
+++ components/lvgl/lv_conf.h
@@ -248,7 +248,7 @@ typedef void * lv_fs_drv_user_data_t;
 
 /*1: Show CPU usage and FPS count in the right bottom corner*/
 #ifndef LV_USE_PERF_MONITOR
-#define LV_USE_PERF_MONITOR     0
+#define LV_USE_PERF_MONITOR     1
 #endif
 /*1: Use the functions and types from the older API if possible */
 #define LV_USE_API_EXTENSION_V6  0
